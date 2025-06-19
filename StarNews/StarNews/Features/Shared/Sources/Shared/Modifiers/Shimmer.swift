4df674f47e8957ca181394f827a7ffafbebf4cb4
Baher Tamer <baher.appledev@gmail.com>
2025-06-19 21:47:53 +0300

    Feature: Add Shared Package (#4)

---
+++ StarNews/StarNews/Features/Shared/Sources/Shared/Modifiers/Shimmer.swift
@@ -0,0 +1,78 @@
+//
+//  Shimmer.swift
+//  SharedUI
+//
+//  Created by Baher Tamer on 19/06/2025.
+//
+
+import SwiftUI
+
+extension View {
+    public func shimmering() -> some View {
+        modifier(Shimmer())
+    }
+}
+
+private struct Shimmer: ViewModifier {
+    // MARK: - Constants
+    private let min: CGFloat = -0.3
+    private let max: CGFloat = 1.3
+    private let animation: Animation = {
+        Animation
+            .linear(duration: 1.5)
+            .delay(0.25)
+            .repeatForever(autoreverses: false)
+    }()
+    private let gradient = Gradient(colors: [
+        .black.opacity(0.3),
+        .black.opacity(0.6),
+        .black.opacity(0.3),
+    ])
+
+    // MARK: - States
+    @State private var isInitialState = true
+    @Environment(\.layoutDirection) private var layoutDirection
+
+    // MARK: - Body
+    public func body(content: Content) -> some View {
+        content
+            .redacted(reason: .placeholder)
+            .mask(linearGradient)
+            .animation(animation, value: isInitialState)
+            .onAppear(perform: toggleState)
+    }
+}
+
+// MARK: - Components
+extension Shimmer {
+    private var linearGradient: some View {
+        LinearGradient(
+            gradient: gradient,
+            startPoint: startPoint,
+            endPoint: endPoint
+        )
+    }
+}
+
+// MARK: - Private Helpers
+extension Shimmer {
+    private var startPoint: UnitPoint {
+        if layoutDirection == .rightToLeft {
+            isInitialState ? UnitPoint(x: max, y: min) : UnitPoint(x: 0, y: 1)
+        } else {
+            isInitialState ? UnitPoint(x: min, y: min) : UnitPoint(x: 1, y: 1)
+        }
+    }
+
+    private var endPoint: UnitPoint {
+        if layoutDirection == .rightToLeft {
+            isInitialState ? UnitPoint(x: 1, y: 0) : UnitPoint(x: min, y: max)
+        } else {
+            isInitialState ? UnitPoint(x: 0, y: 0) : UnitPoint(x: max, y: max)
+        }
+    }
+
+    private func toggleState() {
+        isInitialState = false
+    }
+}

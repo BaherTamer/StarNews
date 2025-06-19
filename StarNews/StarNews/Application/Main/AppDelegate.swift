59a3deb53a6c23b8aba38dfb9edad54c50dc2ba8
Baher Tamer <baher.appledev@gmail.com>
2025-06-19 20:52:28 +0300

    chore: remove unused files & configurations

---
+++ StarNews/StarNews/Application/Main/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  StarNews
+//
+//  Created by Baher Tamer on 19/06/2025.
+//
+
+import UIKit.UIApplication
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        return true
+    }
+}
---
+++ StarNews/StarNews/Application/Main/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  StarNews
+//
+//  Created by Baher Tamer on 19/06/2025.
+//
+
+import UIKit.UIScene
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    // MARK: - Variables
+    var window: UIWindow?
+}
+
+// MARK: - UIWindowSceneDelegate
+extension SceneDelegate {
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        // TODO: Display root view
+    }
+}

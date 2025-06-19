b8269a5b6617130d25e171e2ec442e5826be6759
Baher Tamer <baher.appledev@gmail.com>
2025-06-19 21:33:48 +0300

    feat: create core module files

---
+++ StarNews/Packages/SNCore/Sources/SNCore/Base/Mapper.swift
@@ -0,0 +1,22 @@
+//
+//  Mapper.swift
+//  SNCore
+//
+//  Created by Baher Tamer on 19/06/2025.
+//
+
+import Foundation
+
+open class Mapper<T> {
+    public init() {}
+
+    open func parse(_ data: Data) throws -> T {
+        fatalError("You must implement this function in the subclass")
+    }
+
+    public final func decode<OUTPUT: Decodable>(data: Data) throws -> OUTPUT {
+        let jsonDecoder = JSONDecoder()
+        jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+        return try jsonDecoder.decode(OUTPUT.self, from: data)
+    }
+}

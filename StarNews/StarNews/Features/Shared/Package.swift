84165933eccdb9b0effc6876c6826998beac6e7d
Baher Tamer <baher.appledev@gmail.com>
2025-06-19 21:46:05 +0300

    feat: add shared module files

---
+++ StarNews/StarNews/Features/Shared/Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Shared",
+    platforms: [
+        .iOS(.v26)
+    ],
+    products: [
+        .library(
+            name: "Shared",
+            targets: ["Shared"]
+        ),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/onevcat/Kingfisher.git",
+            .upToNextMajor(from: "8.1.3")
+        )
+    ],
+    targets: [
+        .target(
+            name: "Shared",
+            dependencies: [
+                "Kingfisher"
+            ]
+        ),
+    ]
+)
---
+++ StarNews/StarNews/Features/Shared/Sources/Shared/Components/WebImage.swift
@@ -0,0 +1,47 @@
+//
+//  WebImage.swift
+//  SharedUI
+//
+//  Created by Baher Tamer on 19/06/2025.
+//
+
+import Kingfisher
+import SwiftUI
+
+public struct WebImage: View {
+    // MARK: - Inputs
+    private let urlString: String?
+    private let contentMode: SwiftUI.ContentMode
+    
+    // MARK: - Computed Properties
+    private var imageURL: URL? {
+        URL(string: urlString ?? "")
+    }
+    
+    // MARK: - Life Cycle
+    public init(
+        urlString: String?,
+        contentMode: SwiftUI.ContentMode
+    ) {
+        self.urlString = urlString
+        self.contentMode = contentMode
+    }
+    
+    // MARK: - Body
+    public var body: some View {
+        KFImage(imageURL)
+            .placeholder(progressView)
+            .retry(maxCount: 3, interval: .seconds(5))
+            .resizable()
+            .aspectRatio(contentMode: contentMode)
+    }
+}
+
+// MARK: - Components
+extension WebImage {
+    private func progressView() -> some View {
+        ProgressView()
+            .imageScale(.large)
+            .tint(.primary)
+    }
+}

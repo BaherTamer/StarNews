e2402139dea5281008b1134c97f53c539d656687
Baher Tamer <baher.appledev@gmail.com>
2025-06-21 01:06:57 +0300

    feat: create article card view

---
+++ StarNews/StarNews/Features/ArticlesList/Sources/ArticlesList/Presentation/SubViews/ArticleCardView.swift
@@ -0,0 +1,104 @@
+//
+//  ArticleCardView.swift
+//  ArticlesList
+//
+//  Created by Baher Tamer on 21/06/2025.
+//
+
+import Shared
+import SNCore
+import SwiftUI
+
+struct ArticleCardView: View {
+    // MARK: - Inputs
+    let article: Article
+
+    // MARK: - Body
+    var body: some View {
+        WebImage(
+            urlString: article.imageURL,
+            contentMode: .fill
+        )
+        .frame(
+            maxWidth: .infinity,
+            minHeight: 400,
+            maxHeight: 400
+        )
+        .overlay(content: overlayGradient)
+        .overlay(
+            alignment: .bottomLeading,
+            content: contentContainer
+        )
+        .clipShape(.rect(cornerRadius: 16))
+    }
+}
+
+// MARK: - Components
+extension ArticleCardView {
+    private func overlayGradient() -> LinearGradient {
+        LinearGradient(
+            colors: [
+                .black,
+                .black.opacity(0.75),
+                .clear,
+            ],
+            startPoint: .bottom,
+            endPoint: .top
+        )
+    }
+
+    private func contentContainer() -> some View {
+        VStack(alignment: .leading) {
+            titleText
+            dividerView
+            footerContainer
+        }
+        .padding()
+    }
+
+    private var titleText: some View {
+        Text(article.title)
+            .font(.title2)
+            .fontWeight(.bold)
+            .multilineTextAlignment(.leading)
+    }
+
+    private var dividerView: some View {
+        Divider()
+            .background(.primary)
+            .padding(.bottom)
+    }
+
+    private var footerContainer: some View {
+        LabeledContent(
+            content: shareButton,
+            label: sourceLabel
+        )
+    }
+
+    private func sourceLabel() -> some View {
+        Label(
+            article.siteName,
+            systemImage: "newspaper.fill"
+        )
+        .font(.headline)
+    }
+
+    private func shareButton() -> some View {
+        Button(action: shareArticle) {
+            Image(systemName: "square.and.arrow.up")
+                .font(.title3)
+                .foregroundColor(.secondary)
+        }
+    }
+}
+
+// MARK: - Private Helpers
+extension ArticleCardView {
+    private func shareArticle() {
+        guard
+            let articleURL = URL(string: article.url)
+        else { return }
+        presentShareSheet(url: articleURL)
+    }
+}

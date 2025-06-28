28b5903fc51314b571490694392645d01d90abff
Baher Tamer <baher.appledev@gmail.com>
2025-06-28 11:38:05 +0300

    feat: create shimmering loader for screen

--- StarNews/StarNews/Features/ArticleDetails/Sources/ArticleDetails/Presentation/ArticleDetailsLoader.swift
+++ StarNews/StarNews/Features/ArticleDetails/Sources/ArticleDetails/Presentation/ArticleDetailsLoader.swift
@@ -9,8 +9,51 @@ import Shared
 import SwiftUI
 
 struct ArticleDetailsLoader: View {
+    // MARK: - Constants
+    private let article = ArticleDetails.example
+    
     // MARK: - Body
     var body: some View {
-        ProgressView()
+        AppScrollView {
+            VStack {
+                headerView
+                descriptionView
+                divider
+                bodyView
+                divider
+                actionsView
+            }
+            .shimmering()
+        }
+        .ignoresSafeArea(edges: .top)
+    }
+}
+
+// MARK: - Components
+extension ArticleDetailsLoader {
+    private var headerView: some View {
+        ArticleDetailsHeaderView(article)
+    }
+    
+    private var descriptionView: some View {
+        ArticleDetailsDescriptionView(article)
+            .padding(.horizontal)
+    }
+    
+    private var divider: some View {
+        Divider().padding()
+    }
+    
+    private var bodyView: some View {
+        Text(article.summary)
+            .padding(.horizontal)
+    }
+    
+    private var actionsView: some View {
+        HStack(spacing: 16) {
+            Text(verbatim: "Share Article")
+            Text(verbatim: "Open in Safari")
+        }
+        .padding()
     }
 }

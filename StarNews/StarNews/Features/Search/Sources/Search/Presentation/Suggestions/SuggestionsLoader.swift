4b1d00f9be6661c1eb882fa02efa2dd2ddaf9ec0
Baher Tamer <baher.appledev@gmail.com>
2025-07-03 16:38:49 +0300

    feat: create suggestions basic loader view

---
+++ StarNews/StarNews/Features/Search/Sources/Search/Presentation/Suggestions/SuggestionsLoader.swift
@@ -0,0 +1,18 @@
+//
+//  SuggestionsLoader.swift
+//  Search
+//
+//  Created by Baher Tamer on 03/07/2025.
+//
+
+import SwiftUI
+
+struct SuggestionsLoader: View {
+    var body: some View {
+        ProgressView()
+            .frame(
+                maxWidth: .infinity,
+                maxHeight: .infinity
+            )
+    }
+}

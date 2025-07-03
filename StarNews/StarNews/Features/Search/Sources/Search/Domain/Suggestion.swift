89f9190172009e2047e3fe2ef9a392cfd609dc2b
Baher Tamer <baher.appledev@gmail.com>
2025-07-03 16:17:54 +0300

    feat: create suggestion model

---
+++ StarNews/StarNews/Features/Search/Sources/Search/Domain/Suggestion.swift
@@ -0,0 +1,29 @@
+//
+//  Suggestion.swift
+//  Search
+//
+//  Created by Baher Tamer on 03/07/2025.
+//
+
+struct Suggestion {
+    let id: Int
+    let title: String
+}
+
+// MARK: - Dummy Data
+extension Suggestion {
+    public static let dummyList: [Suggestion] = [
+        Suggestion(
+            id: 28823,
+            title: "GITAI expanding satellite product line after successful demo"
+        ),
+        Suggestion(
+            id: 28822,
+            title: "COMING SOON: 2024 Annual Highlights of Results from the International Space Station"
+        ),
+        Suggestion(
+            id: 28820,
+            title: "Live coverage: SpaceX to launch 21 Starlink satellites on Falcon 9 rocket from Cape Canaveral"
+        ),
+    ]
+}

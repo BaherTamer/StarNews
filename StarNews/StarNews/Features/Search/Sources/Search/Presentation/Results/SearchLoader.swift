6f4bb8b8b03ca3333f0110471d76fcd679b00aa9
Baher Tamer <baher.appledev@gmail.com>
2025-07-05 00:55:27 +0300

    feat: create basic search loader file

---
+++ StarNews/StarNews/Features/Search/Sources/Search/Presentation/Results/SearchLoader.swift
@@ -0,0 +1,14 @@
+//
+//  SearchLoader.swift
+//  Search
+//
+//  Created by Baher Tamer on 05/07/2025.
+//
+
+import SwiftUI
+
+struct SearchLoader: View {
+    var body: some View {
+        ProgressView()
+    }
+}

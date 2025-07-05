900ef1487dc9a83d032bf49dd6669ddb33f7d5f1
Baher Tamer <baher.appledev@gmail.com>
2025-07-05 14:35:36 +0300

    feat: create search loader ui

--- StarNews/StarNews/Features/Search/Sources/Search/Presentation/Results/SearchLoader.swift
+++ StarNews/StarNews/Features/Search/Sources/Search/Presentation/Results/SearchLoader.swift
@@ -5,10 +5,33 @@
 //  Created by Baher Tamer on 05/07/2025.
 //
 
+import Shared
+import SNDesignSystem
 import SwiftUI
 
 struct SearchLoader: View {
+    // MARK: - Body
     var body: some View {
-        ProgressView()
+        AppScrollView {
+            VStack(spacing: Spaces.s16) {
+                ForEach(
+                    0..<10,
+                    id: \.self,
+                    content: searchPlaceholder
+                )
+            }
+            .padding(.horizontal)
+        }
+    }
+}
+
+// MARK: - Components
+extension SearchLoader {
+    private func searchPlaceholder(_ index: Int) -> some View {
+        SearchCardView(
+            result: SearchResult.dummyList.first!,
+            onTap: {}
+        )
+        .shimmering()
     }
 }

ba13cb792f8bce6c7757efb2ee58bc9bdfab8fb4
Baher Tamer <baher.appledev@gmail.com>
2025-07-05 14:35:49 +0300

    refactor: shared module local dependencies setup

--- StarNews/StarNews/Features/Shared/Package.swift
+++ StarNews/StarNews/Features/Shared/Package.swift
@@ -3,33 +3,65 @@
 
 import PackageDescription
 
+enum Module: String, CaseIterable {
+    case shared = "Shared"
+    case core = "SNCore"
+    case designSystem = "SNDesignSystem"
+
+    var path: String {
+        "../../Packages/"
+    }
+}
+
+private var allModules: [Module] {
+    Module
+        .allCases
+        .filter({ $0 != .shared })
+}
+
+// MARK: - Package Local Dependancies
+private var dependencies: [Package.Dependency] {
+    let dependency = Package.Dependency.self
+    return allModules.map({
+        dependency.package(
+            name: $0.rawValue,
+            path: "\($0.path)\($0.rawValue)"
+        )
+    })
+}
+
+private var targets: [PackageDescription.Target.Dependency] {
+    let dependency = PackageDescription.Target.Dependency.self
+    return allModules.map({
+        dependency.byName(name: $0.rawValue)
+    })
+}
+
+// MARK: - Package
 let package = Package(
-    name: "Shared",
+    name: Module.shared.rawValue,
     platforms: [
         .iOS(.v26)
     ],
     products: [
         .library(
-            name: "Shared",
-            targets: ["Shared"]
+            name: Module.shared.rawValue,
+            targets: [
+                Module.shared.rawValue
+            ]
         ),
     ],
-    dependencies: [
+    dependencies: dependencies + [
         .package(
             url: "https://github.com/onevcat/Kingfisher.git",
             .upToNextMajor(from: "8.1.3")
         ),
-        .package(
-            name: "SNDesignSystem",
-            path: "../../Packages/SNDesignSystem"
-        )
     ],
     targets: [
         .target(
-            name: "Shared",
-            dependencies: [
+            name: Module.shared.rawValue,
+            dependencies: targets + [
                 "Kingfisher",
-                "SNDesignSystem"
             ]
         ),
     ]

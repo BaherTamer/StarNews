12912d9d9aef2d9e84b29dccfbf7a44c8b56b562
Baher Tamer <baher.appledev@gmail.com>
2025-07-15 11:49:40 +0300

    test: search suggestions use case

---
+++ StarNews/StarNews/Features/Search/Tests/Suggestions/Tests/UseCases/SuggestionsUseCaseTests.swift
@@ -0,0 +1,47 @@
+//
+//  SuggestionsUseCaseTests.swift
+//  Search
+//
+//  Created by Baher Tamer on 14/07/2025.
+//
+
+import Testing
+@testable import Search
+
+@MainActor
+final class SuggestionsUseCaseTests {
+    // MARK: - Variables
+    private let query: String
+    private let useCase: SuggestionsUseCase!
+    private var repository: TestableSuggestionsRepository!
+    
+    // MARK: - Life Cycle
+    init() {
+        self.query = "Rocket"
+        self.repository = StubSuggestionsRepository()
+        self.useCase = DefaultSuggestionsUseCase(repository: repository)
+    }
+    
+    // MARK: - Tests
+    
+    @Test func getSuggestionsSuccess() async throws {
+        // Given
+        let response = try await useCase.execute(query: query)
+        
+        // When
+        let suggestions = try await useCase.execute(query: query)
+        
+        // Then
+        #expect(suggestions.count == Suggestion.dummyList.count)
+    }
+    
+    @Test func getSuggestionsFailure() async throws {
+        // Given
+        repository.shouldThrowError = true
+        
+        // Then
+        await #expect(throws: SuggestionsError.repositoryError) {
+            _ = try await useCase.execute(query: query)
+        }
+    }
+}
